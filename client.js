af14e26c77f16de9d9b5b20f42eb8ce4c9a4ec26
tidjungs <waewprach.s@ku.th>
2018-09-12 22:41:00 +0700

    add README & test script

--- client.js
+++ client.js
@@ -2,10 +2,6 @@ const WebSocket = require('ws');
 
 const ws = new WebSocket('ws://localhost:8080');
 
-ws.on('open', function open() {
-  ws.send('something');
-});
-
 ws.on('message', function incoming(data) {
   console.log(data);
 });
